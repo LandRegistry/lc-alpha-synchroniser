ad8576c763149b2aa1441354106418145c869756
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-13 11:02:50 +0100

    Legacyfication of data, part 1

--- application/listener.py
+++ application/listener.py
@@ -1,8 +1,32 @@
 import sys
+from application.utility import encode_name, occupation_string, residences_to_string
 
 
 def message_received(body, message):
-    print("Received: " + str(body))
     print("Received Msg: " + str(message))
+
+    encoded_debtor_name = encode_name(body['debtor_name'])
+    converted = {
+        'time': 'TODO',
+        'registration_no': 'TODO',
+        'priority_notice': '',
+        'reverse_name': encoded_debtor_name['coded_name'],
+        'property_county': 255,
+        'registration_date': body['date'],
+        'class_type': body['application_type'],
+        'remainder_name': encoded_debtor_name['remainder_name'],
+        'punctuation_code': encoded_debtor_name['hex_code'],
+        'name': '',
+        'address': residences_to_string(body).upper(),
+        'occupation': occupation_string(body).upper(),
+        'counties': '',
+        'amendment_info': 'Insolvency Service Ref. ' + body['application_ref'], # TODO: somewhat assumed its always INS
+        'property': '',
+        'parish_district': '',
+        'priority_notice_ref': ''
+    }
+
+    # TODO: send to legacy-db and handle results
+    print(converted)
     message.ack()
     sys.stdout.flush()
---
+++ application/utility.py
@@ -0,0 +1,69 @@
+import re
+
+
+def encode_name(name):
+    codes_are = {'&': 0, ' ': 1, '-': 2, "'": 3, '(': 4, ')': 5, '*': 6, '?': 7}
+
+    mash_with_punc = ' '.join(name['forenames']) + '*' + name['surname']
+    mashed = ""
+    codes = ""
+
+    search = re.search("'|\s|\*", mash_with_punc)
+    while search is not None:
+        index = search.start()
+        word = mash_with_punc[0:index]
+        punc = mash_with_punc[index]
+        punc = codes_are[punc]
+
+        length = index
+        mash_with_punc = mash_with_punc[index + 1:]
+        mashed += word
+        code = (punc << 5) + length
+        codes += '{:02x}'.format(code)
+
+        search = re.search("'|\s|\*", mash_with_punc)
+
+    mashed += mash_with_punc
+
+    last_12_chars = mashed[-12:]
+    first_chars = mashed[:-12]
+
+    return {
+        'coded_name': last_12_chars.upper()[::-1],
+        'remainder_name': first_chars.upper(),
+        'hex_code': codes.upper()
+    }
+
+
+def address_to_string(address):
+    # TODO: consider implications of current data being <lines>\<postcode>\<county>,
+    # where this is storing <lines>\<county>\<postcode>
+    lines = ' '.join(address['address_lines']) + ' ' + address['postcode']
+    return lines
+
+
+def residences_to_string(data):
+    addresses = ""
+    for address in data['residence']:
+        addresses += address_to_string(address) + "  "
+    return addresses.strip()
+
+
+def name_to_string(name):
+    return ' '.join(name['forenames']) + ' ' + name['surname']
+
+
+def occupation_string(data):
+    # ("(N/A) <AKA foo>+ [T/A <trading name> AS]? <occupation>")
+    n_a = "(N/A)"
+
+    alias_names = ""
+    for name in data['debtor_alternative_name']:
+        alias_names += " " + name_to_string(name).upper()
+
+    if 'trading_name' in data and data['trading_name'] != '':
+        occu = " T/A " + data['trading_name'] + " AS " + data['occupation']
+    else:
+        occu = " " + data['occupation']
+
+    return "{}{}{}".format(n_a, alias_names, occu)
