7cec81ebf9a43bfbc40a890f561c70caaba8de3d
Steve Millis <steve.millis@digital.landregistry.gov.uk>
2015-09-02 09:33:08 +0100

    Change debtor_alias to alternative_name

--- application/utility.py
+++ application/utility.py
@@ -65,7 +65,7 @@ def occupation_string(data):
     n_a = "(N/A)"
 
     alias_names = ""
-    for name in data['debtor_alias']:
+    for name in data['debtor_alternative_name']:
         alias_names += " " + name_to_string(name)
 
     if alias_names != "":
