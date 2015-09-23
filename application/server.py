d5ca846fe56a9e111f965ef0e10ea15b58fe3dd7
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-09-23 09:12:33 +0100

    PyLint cleanup

--- application/server.py
+++ application/server.py
@@ -3,9 +3,7 @@ from application.listener import message_received, listen
 import kombu
 from kombu.common import maybe_declare
 from amqp import AccessRefused
-from flask import Response
 import logging
-import threading
 
 
 def setup_incoming(hostname):
@@ -25,20 +23,20 @@ def setup_incoming(hostname):
         queue.declare()
     except AccessRefused:
         logging.error("Access Refused")
-    logging.debug("queue name, exchange, binding_key: {}, {}, {}".format(queue.name, queue.exchange, queue.routing_key))
+    logging.debug("queue name, exchange, binding_key: %s, %s, %s", queue.name, queue.exchange, queue.routing_key)
 
     consumer = kombu.Consumer(channel, queues=queue, callbacks=[message_received], accept=['json'])
     consumer.consume()
 
-    logging.debug('channel_id: {}'.format(consumer.channel.channel_id))
-    logging.debug('queue(s): {}'.format(consumer.queues))
+    logging.debug('channel_id: %s', consumer.channel.channel_id)
+    logging.debug('queue(s): %s', consumer.queues)
     return connection, consumer
 
 
 def setup_error_queue(hostname):
     connection = kombu.Connection(hostname=hostname)
     producer = connection.SimpleQueue('sync_error')
-    return connection, producer
+    return producer
 
 
 def run():
@@ -46,7 +44,7 @@ def run():
     hostname = "amqp://{}:{}@{}:{}".format(app.config['MQ_USERNAME'], app.config['MQ_PASSWORD'],
                                            app.config['MQ_HOSTNAME'], app.config['MQ_PORT'])
     incoming_connection, incoming_consumer = setup_incoming(hostname)
-    error_connection, error_producer = setup_error_queue(hostname)
+    error_producer = setup_error_queue(hostname)
 
     listen(incoming_connection, error_producer)
     incoming_consumer.close()
