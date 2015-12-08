3d1a57ccaf268f98908c24f2de0f4c799b5b01f0
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-12-08 14:15:31 +0000

    Sync cancellations

--- application/listener.py
+++ application/listener.py
@@ -4,6 +4,7 @@ import requests
 import json
 import datetime
 import logging
+import re
 
 
 class SynchroniserError(Exception):
@@ -20,11 +21,12 @@ def create_legacy_data(data):
     encoded_debtor_name = encode_name(data['debtor_name'])
     return {
         'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f'),
-        'registration_no': str(data['registration_no']).rjust(8),
+        'registration_no': str(data['registration']['number']).rjust(8),
         'priority_notice': '',
         'reverse_name': encoded_debtor_name['coded_name'],
         'property_county': 255,  # Always 255 for a bankruptcy.
-        'registration_date': data['registration_date'],
+                    # TODO: sort out county thing
+        'registration_date': data['registration']['date'],
         'class_type': app_type,
         'remainder_name': encoded_debtor_name['remainder_name'],
         'punctuation_code': encoded_debtor_name['hex_code'],
@@ -39,11 +41,65 @@ def create_legacy_data(data):
     }
 
 
+def get_registration(number, year):
+    response = requests.get(app.config['REGISTER_URI'] + '/registrations/' + str(number), params={'year': year})
+    if response.status_code != 200:
+        raise SynchroniserError('/registrations - ' + str(response.status_code))
+    return response.json()
+
+
+def create_cancellation_history(body, regn):
+    print(body)
+    print(regn)
+    class_of_charge = re.sub("\(|\)", "", regn['application_type'])
+    cancellation = {
+        'class': class_of_charge,
+        'reg_no': regn['registration']['number'],
+        'date': regn['registration']['date'],
+        'template': 'Cancellation',
+        'text': 'Cancelled by Land Charge reference number(s) {}, on {}'.format(
+            regn['cancellation_ref'],
+            regn['cancellation_date']  # TODO - needs translation???
+        )
+    }
+
+    uri = '/history_notes/{}/{}/{}'.format(regn['registration']['number'], regn['cancellation_date'], regn['application_type'])
+    response = requests.post(app.config['LEGACY_DB_URI'] + uri, data=json.dumps(cancellation), headers={'Content-Type': 'application/json'})
+    logging.info('POST %s - %d', uri, response.status_code)
+    return response.status_code
+
+
+def create_amendment_history(regn):
+    class_of_charge = re.sub("\(|\)", "", regn['application_type'])
+    template = 'Amend ' + class_of_charge
+
+    amendment = {
+        'class': class_of_charge,
+        'reg_no': regn['amends_regn']['number'],
+        'date': regn['amends_regn']['date'],
+        'template': template,
+        'text': 'Please note that the registration number %d, dated %s, has been amended by Land Charge '
+                'reference number(s) %d, dated %s. Copies of these registrations are enclosed.'.format(
+            regn['amends_regn']['number'],
+            regn['amends_regn']['date'],
+            regn['registration']['number'],
+            regn['registration']['date']
+        )
+    }
+
+    uri = '/history_notes/%s/%s/%s'.format(regn['amends_regn']['number'], regn['amends_regn']['date'], regn['application_type'])
+    response = requests.post(uri, data=json.dumps(amendment), headers={'Content-Type': 'application/json'})
+    logging.info('POST %s - %s', uri, response.status_code)
+    return response.status_code
+
+
 def receive_new_regs(errors, body):
-    for number in body['data']:
-        response = requests.get(app.config['REGISTER_URI'] + '/registrations/' + str(number))
+    for application in body['data']:
+        number = application['number']
+        date = application['date']
+        response = requests.get(app.config['REGISTER_URI'] + '/registrations/' + date + '/' + str(number))
         if response.status_code != 200:
-            logging.error("GET /registrations/%d - %s", number, response.status_code)
+            logging.error("GET /registrations/{} - {}", number, response.status_code)
             errors.append({
                 "uri": '/registrations',
                 "status_code": response.status_code,
@@ -51,14 +107,15 @@ def receive_new_regs(errors, body):
             })
         else:
             logging.debug('Registration retrieved')
-            converted = create_legacy_data(response.json())
+            body = response.json()
+            converted = create_legacy_data(body)
 
             put_response = requests.put(app.config['LEGACY_DB_URI'] + '/land_charges',
                                         data=json.dumps(converted), headers={'Content-Type': 'application/json'})
             if put_response.status_code == 200:
                 logging.debug('PUT /land_charges - OK')
             else:
-                logging.error('PUT /land_charges - %s', put_response.status_code)
+                logging.error('PUT /land_charges - {}', put_response.status_code)
                 errors.append({
                     "uri": '/land_charge',
                     "status_code": put_response.status_code,
@@ -66,12 +123,48 @@ def receive_new_regs(errors, body):
                     "registration_no": number
                 })
 
+            coc = body['application_type']
+            create_document_row("/{}/{}/{}".format(number, date, coc), number, date, body)
+
+
+def create_document_row(resource, reg_no, reg_date, body):
+    doc_row = {
+        'class': body['application_type'],
+        'reg_no': reg_no,
+        'date': reg_date,
+        'orig_class': body['application_type'],
+        'orig_no': body['registration']['number'],
+        'orig_date': body['registration']['date'],
+        'canc_ind': '',
+        'app_type': 'CN'
+    }
+    put = requests.put(app.config['LEGACY_DB_URI'] + '/doc_info' + resource,
+                       data=json.dumps(doc_row),
+                       headers={'Content-Type': 'application/json'})
+    logging.info('POST /doc_info - %s', str(put.status_code))
+    if put.status_code != 200:
+        raise SynchroniserError('POST /doc_info - ' + str(put.status_code))
+
 
 def receive_cancellation(errors, body):
-    # Delete from from land_charges
-    # Write out endorsement
-    # Add entry to doc_info
-    pass
+    for ref in body['data']:
+        number = ref['number']
+        date = ref['date']
+        response = requests.get(app.config['REGISTER_URI'] + '/registrations/' + date + '/' + str(number))
+        if response.status_code != 200:
+            logging.error('GET /registrations - %s', str(response.status_code))
+            raise SynchroniserError('GET /registrations - ' + str(response.status_code))
+        else:
+            regn = response.json()
+            resource = "/{}/{}/{}".format(regn['registration']['number'],
+                                          regn['registration']['date'],
+                                          regn['application_type'])
+            delete = requests.delete(app.config['LEGACY_DB_URI'] + '/land_charges' + resource)
+            if delete.status_code != 200:
+                logging.error('DELETE /land_charges - %s', str(delete.status_code))
+                raise SynchroniserError('DELETE /land_charges - ' + str(delete.status_code))
+            create_cancellation_history(body, regn)
+            create_document_row(resource, regn['cancellation_ref'], regn['cancellation_date'], regn)
 
 
 def receive_update(errors, body):
