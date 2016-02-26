228e43aa57ac837aa15efa0edb837ea52ca842e5
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-02-26 14:24:37 +0000

    Some test assistance scripts

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ class Config(object):
     DEBUG = os.getenv('DEBUG', True)
     APPLICATION_NAME = "lc-synchroniser"
     REGISTER_URI = os.getenv('LAND_CHARGES_URL', "http://localhost:5004")
-    LEGACY_DB_URI = os.getenv('LEGACY_ADAPTER_URL', "http://10.0.2.2:15007")#"http://localhost:5007")
+    #LEGACY_DB_URI = os.getenv('LEGACY_ADAPTER_URL', "http://10.0.2.2:15007")
+    LEGACY_DB_URI = os.getenv('LEGACY_ADAPTER_URL', "http://localhost:5007")
     CASEWORK_API_URI = os.getenv('CASEWORK_API_URL', "http://localhost:5006")
     MQ_USERNAME = os.getenv("MQ_USERNAME", "mquser")
     MQ_PASSWORD = os.getenv("MQ_PASSWORD", "mqpassword")
